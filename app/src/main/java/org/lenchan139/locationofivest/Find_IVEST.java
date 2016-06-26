aee540d8ee59ea82bf542943fc1b1ad4b2c989b5
Len Chan <mail@lenchan139.org>
2016-06-26 12:17:04 +0800

    .

--- app/src/main/java/org/lenchan139/locationofivest/Find_IVEST.java
+++ app/src/main/java/org/lenchan139/locationofivest/Find_IVEST.java
@@ -14,13 +14,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity
+public class Find_IVEST extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_find_ivest);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
